51c162077cd43f4db2388c89ffb33f57899d2850
Kenny Chow <68889558+kennychowpd@users.noreply.github.com>
2023-12-18 13:24:17 -0500

    feat: CRUD for users

---
+++ routes/users.js
@@ -0,0 +1,54 @@
+import { Router } from 'express';
+import * as db from '../db/index.js';
+
+const router = new Router();
+
+// GET A USER
+const getUserByFirebaseId = async (request, response) => {
+  const { firebase_uid } = request.body;
+  const sql = `SELECT firebase_uid, email, username FROM Users WHERE firebase_uid = $1;`;
+
+  const result = await db.query(sql, [firebase_uid]).catch((error) => {
+    console.log(error);
+  });
+  response.send(result.rows[0]);
+};
+router.get('/', getUserByFirebaseId);
+
+// CREATE A USER
+const createUser = async (request, response) => {
+  const { firebase_uid, email, username } = request.body;
+  const sql = `INSERT INTO Users (firebase_uid, email, username) VALUES($1, $2, $3) RETURNING firebase_uid;`;
+
+  const result = await db.query(sql, [firebase_uid, email, username]).catch((error) => {
+    console.log(error);
+  });
+  response.send(result.rows[0]);
+};
+router.post('/', createUser);
+
+// UPDATE A USER
+const updateUser = async (request, response) => {
+  const { firebase_uid, email, username } = request.body;
+  const sql = `UPDATE Users SET email = $2, username = $3 WHERE firebase_uid = $1 RETURNING firebase_uid;`;
+
+  const result = await db.query(sql, [firebase_uid, email, username]).catch((error) => {
+    console.log(error);
+  });
+  response.send(result.rows[0]);
+};
+router.put('/', updateUser);
+
+// DELETE
+const deleteUser = async (request, response) => {
+  const { firebase_uid } = request.body;
+  const sql = `DELETE FROM Users WHERE firebase_uid = $1 RETURNING username;`;
+
+  const result = await db.query(sql, [firebase_uid]).catch((error) => {
+    console.log(error);
+  });
+  response.send(result.rows[0]);
+};
+router.delete('/', deleteUser);
+
+export default router;
